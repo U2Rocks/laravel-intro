05d368ea4bc78bce24545935f645f1ed6828fc17
unknown <tffry@coastal.edu>
2020-02-12 14:00:53 -0500

    Updated user model so it can have many reservations

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function User() {
+        return $this->hasMany('app\Models\Reservation');
+    }
 }
